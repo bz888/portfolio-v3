cbb481edf8da99ef35b5d4d2bc5755797ed09a4d
bz888 <ziyu.ben158@gmail.com>
2025-01-15 22:32:55 +1300

    update home text bkpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
   return (
     <div className='z-0 w-screen h-screen flex flex-col'>
-      <p className='text-6xl pt-6'>Full-stack</p>
-      <p className='text-6xl'>UI Design</p>
-      <p className='text-6xl pt-6'>I am a young developer with a passion for tech and a mission to present the endless possibilites of web development, I am always in pursuit of new knowledge through experiments and projects.</p>
+      <p className='text-4xl sm:text-6xl pt-6'>Full-stack</p>
+      <p className='text-4xl sm:text-6xl'>UI Design</p>
+      <p className='text-4xl sm:text-6xl pt-6'>I am a young developer with a passion for tech and a mission to present the endless possibilites of web development, I am always in pursuit of new knowledge through experiments and projects.</p>
     </div>
   )
 }
